952a7c46583ce1fa0f3366237bc9e9d9bbea3279
PotatoFazi <rfaraz51@gmail.com>
2022-06-24 03:12:22 +0500

    Compare Screen Completed

--- Components/Compare.js
+++ Components/Compare.js
@@ -1,11 +1,96 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { CryptoState } from "../CryptoContext";
+import { darkColor, lightColor } from "../Colors";
 
-const Compare = () => {
+const Compare = ({
+  name,
+  image,
+  price,
+  coinSymbol,
+  newPrice,
+  currency,
+  newCurrency,
+  currencyIcon,
+}) => {
+  const { symbol, theme } = CryptoState();
+
   return (
-    <View>
-      <Text>Compare</Text>
-    </View>
+    <>
+      <View
+        style={{
+          marginVertical: "3%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <View
+          style={{
+            width: "30%",
+            // alignItems: "center",
+            // justifyContent: "center",
+            flexDirection: "row",
+          }}
+        >
+          <Image source={{ uri: image }} style={{ width: 38, height: 38 }} />
+          <View style={{ paddingLeft: 10, justifyContent: "center" }}>
+            <Text
+              style={{
+                color:
+                  theme === "light"
+                    ? lightColor.fontColor
+                    : darkColor.fontColor,
+                // fontWeight: "bold",
+              }}
+            >
+              {name}
+            </Text>
+            <Text
+              style={{
+                color:
+                  theme === "light"
+                    ? lightColor.fontColor
+                    : darkColor.fontColor,
+              }}
+            >
+              {coinSymbol.toUpperCase()}
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            width: "30%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              color:
+                theme === "light" ? lightColor.fontColor : darkColor.fontColor,
+            }}
+          >
+            {symbol} {price}
+          </Text>
+        </View>
+        <View
+          style={{
+            width: "30%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              color:
+                theme === "light" ? lightColor.fontColor : darkColor.fontColor,
+            }}
+          >
+            {currencyIcon} {newPrice}
+          </Text>
+        </View>
+      </View>
+    </>
   );
 };
 
--- Screens/CompareScreen.js
+++ Screens/CompareScreen.js
@@ -14,9 +14,44 @@ import Compare from "../Components/Compare";
 
 const CompareScreen = () => {
   const [trendingCoins, setTrendingCoins] = useState();
-  const [selectedCoin, setSelectedCoin] = useState();
+  const [selectedCoin, setSelectedCoin] = useState(null);
   const { currency, theme } = CryptoState();
   const { setCurrency } = CryptoState();
+  const [currencyIcon, setCurrencyIcon] = useState();
+  const [selectedCurrency, setSelectedCurrency] = useState("usd");
+
+  useEffect(() => {
+    if (selectedCurrency === "pkr") setCurrencyIcon("Rs");
+    else if (selectedCurrency === "usd") setCurrencyIcon("$");
+    else if (selectedCurrency === "inr") setCurrencyIcon("₹");
+    else if (selectedCurrency === "eur") setCurrencyIcon("€");
+    else if (selectedCurrency === "gbp") setCurrencyIcon("£");
+    else if (selectedCurrency === "cad") setCurrencyIcon("$");
+    else if (selectedCurrency === "aud") setCurrencyIcon("$");
+    else if (selectedCurrency === "brl") setCurrencyIcon("R$");
+    else if (selectedCurrency === "cny") setCurrencyIcon("¥");
+    else if (selectedCurrency === "rub") setCurrencyIcon("₽");
+    else if (selectedCurrency === "idr") setCurrencyIcon("Rp");
+    else if (selectedCurrency === "mxn") setCurrencyIcon("$");
+    else if (selectedCurrency === "ils") setCurrencyIcon("₪");
+    else if (selectedCurrency === "jpy") setCurrencyIcon("¥");
+    else if (selectedCurrency === "nzd") setCurrencyIcon("$");
+    else if (selectedCurrency === "nok") setCurrencyIcon("kr");
+    else if (selectedCurrency === "sek") setCurrencyIcon("kr");
+    else if (selectedCurrency === "chf") setCurrencyIcon("Fr");
+    else if (selectedCurrency === "sgd") setCurrencyIcon("$");
+    else if (selectedCurrency === "thb") setCurrencyIcon("฿");
+    else if (selectedCurrency === "twd") setCurrencyIcon("NT$");
+    else if (selectedCurrency === "zar") setCurrencyIcon("R");
+    else if (selectedCurrency === "bgn") setCurrencyIcon("лв");
+    else if (selectedCurrency === "hkd") setCurrencyIcon("$");
+    else if (selectedCurrency === "php") setCurrencyIcon("₱");
+    else if (selectedCurrency === "try") setCurrencyIcon("₺");
+    else if (selectedCurrency === "uah") setCurrencyIcon("₴");
+    else if (selectedCurrency === "czk") setCurrencyIcon("Kč");
+    else if (selectedCurrency === "pln") setCurrencyIcon("zł");
+  }, [selectedCurrency]);
+
   const data = [
     // { label: "USD", value: "usd" },
     { label: "PKR", value: "pkr" },
@@ -74,17 +109,12 @@ const CompareScreen = () => {
         console.log(error);
       });
     const filteredData = data.map((coin) => {
-      const temp = {
-        logo: coin.image,
-        label: coin.name,
-        symbol: coin.symbol,
-        price: coin.current_price,
-      };
+      const temp = { newPrice: coin.current_price };
       return temp;
     });
     setSelectedCoin(filteredData);
+    // console.log(selectedCoin);
   };
-  const [selectedCurrency, setSelectedCurrency] = useState("usd");
 
   useEffect(() => {
     fetch();
@@ -141,13 +171,49 @@ const CompareScreen = () => {
           <View
             style={{ flexDirection: "row", justifyContent: "space-between" }}
           >
-            <View style={{ alignItems: "center", justifyContent: "center" }}>
-              <Text>Name</Text>
+            <View
+              style={{
+                width: "30%",
+                // alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text
+                style={{
+                  color:
+                    theme === "light"
+                      ? lightColor.fontColor
+                      : darkColor.fontColor,
+                }}
+              >
+                Name
+              </Text>
             </View>
-            <View style={{ alignItems: "center", justifyContent: "center" }}>
-              <Text>{currency.toUpperCase()}</Text>
+            <View
+              style={{
+                width: "30%",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text
+                style={{
+                  color:
+                    theme === "light"
+                      ? lightColor.fontColor
+                      : darkColor.fontColor,
+                }}
+              >
+                {currency.toUpperCase()}
+              </Text>
             </View>
-            <View style={{ alignItems: "center", justifyContent: "center" }}>
+            <View
+              style={{
+                width: "30%",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <RNPickerSelect
                 placeholder={placeholder}
                 items={data}
@@ -155,30 +221,77 @@ const CompareScreen = () => {
                   console.log(value);
                   setSelectedCurrency(value);
                 }}
-                style={pickerSelectStyles}
+                style={{
+                  inputIOS: {
+                    color:
+                      theme === "light"
+                        ? lightColor.fontColor
+                        : darkColor.fontColor,
+                    fontSize: 16,
+                    textAlign: "center",
+                    paddingVertical: 12,
+                    paddingHorizontal: 10,
+                    borderWidth: 1,
+                    borderColor: "gray",
+                    borderRadius: 4,
+                    // color: "black",
+                    width: "100%",
+                    marginRight: 30,
+                  },
+                  inputAndroid: {
+                    color:
+                      theme === "light"
+                        ? lightColor.fontColor
+                        : darkColor.fontColor,
+
+                    fontSize: 16,
+                    paddingHorizontal: 10,
+                    paddingVertical: 8,
+                    borderWidth: 0.5,
+                    borderColor: "purple",
+                    borderRadius: 8,
+                    // color: "black",
+                    paddingRight: 30,
+                  },
+                }}
                 value={selectedCurrency}
               />
             </View>
           </View>
-
-          <ScrollView
-            showsVerticalScrollIndicator={false}
-            // refreshControl={
-            //   <RefreshControl
-            //     refreshing={refresh}
-            //     onRefresh={fetchCoins}
-            //     colors={["#236AF3"]}
-            //   />
-            // }
-          >
-            {/* {trendingCoins &&
-              trendingCoins.map((coin) => (
-                <View>
-                  <Text>{coin.id}</Text>
-                </View>
-              ))} */}
-            <Compare />
-          </ScrollView>
+          <View style={{ height: "70%" }}>
+            <ScrollView
+              showsVerticalScrollIndicator={false}
+              // contentContainerStyle={{ height: "100%" }}
+              // refreshControl={
+              //   <RefreshControl
+              //     refreshing={refresh}
+              //     onRefresh={fetchCoins}
+              //     colors={["#236AF3"]}
+              //   />
+              // }
+            >
+              {trendingCoins &&
+                trendingCoins.map((coin, i) => (
+                  <Compare
+                    image={coin.logo}
+                    name={
+                      coin.label.length > 9
+                        ? coin.label.slice(0, 8) + ".."
+                        : coin.label
+                    }
+                    price={coin.price}
+                    coinSymbol={coin.symbol}
+                    newPrice={
+                      selectedCoin == null
+                        ? coin.price
+                        : selectedCoin[i].newPrice
+                    }
+                    currency={currency}
+                    currencyIcon={currencyIcon}
+                  />
+                ))}
+            </ScrollView>
+          </View>
         </View>
       </SafeAreaView>
     </View>
@@ -217,6 +330,7 @@ const styles = StyleSheet.create({
 const pickerSelectStyles = StyleSheet.create({
   inputIOS: {
     fontSize: 16,
+    textAlign: "center",
     paddingVertical: 12,
     paddingHorizontal: 10,
     borderWidth: 1,
@@ -224,7 +338,7 @@ const pickerSelectStyles = StyleSheet.create({
     borderRadius: 4,
     color: "black",
     width: "100%",
-    paddingRight: 30, // to ensure the text is never behind the icon
+    marginRight: 30,
   },
   inputAndroid: {
     fontSize: 16,
@@ -234,6 +348,6 @@ const pickerSelectStyles = StyleSheet.create({
     borderColor: "purple",
     borderRadius: 8,
     color: "black",
-    paddingRight: 30, // to ensure the text is never behind the icon
+    paddingRight: 30,
   },
 });
